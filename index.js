029b511564d3984b466fbac4428cae191d84ca6d
Scott Lin <scott.lin@coolbe.com.tw>
2020-03-28 17:29:15 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    let str = 'Hello World! 1.1';
+    res.send(str);
+    console.log(str);
 });
 
 var server = app.listen(3000, function () {
